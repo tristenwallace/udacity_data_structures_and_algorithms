1289c405a57c8d3d03f6017ff512b03a7f710dab
tristenwallace <tri3sten@gmail.com>
2024-02-21 10:57:28 -0500

    feat: Solve task 1

--- intro_problems/Task1.py
+++ intro_problems/Task1.py
@@ -18,3 +18,18 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+tel_list = []
+
+for _, text in enumerate(texts):
+    if text[0] not in tel_list:
+        tel_list.append(text[0])
+    if text[1] not in tel_list:
+        tel_list.append(text[1])
+
+for _, call in enumerate(calls):
+    if call[0] not in tel_list:
+        tel_list.append(call[0])
+    if call[1] not in tel_list:
+        tel_list.append(call[1])
+
+print(f"There are {len(tel_list)} different telephone numbers in the records.")
