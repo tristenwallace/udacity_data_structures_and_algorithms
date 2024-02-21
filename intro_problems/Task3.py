234f4dcf4c2d718856997a56d6f49af3218b4966
tristenwallace <tri3sten@gmail.com>
2024-02-21 15:01:14 -0500

    feat: Solve task 3

--- intro_problems/Task3.py
+++ intro_problems/Task3.py
@@ -21,17 +21,17 @@ have the form (080)xxxxxxx.)
 Part A: Find all of the area codes and mobile prefixes called by people
 in Bangalore. In other words, the calls were initiated by "(080)" area code
 to the following area codes and mobile prefixes:
- - Fixed lines start with an area code enclosed in brackets. The area
-   codes vary in length but always begin with 0.
- - Mobile numbers have no parentheses, but have a space in the middle
-   of the number to help readability. The prefix of a mobile number
-   is its first four digits, and they always start with 7, 8 or 9.
- - Telemarketers' numbers have no parentheses or space, but they start
-   with the area code 140.
+- Fixed lines start with an area code enclosed in brackets. The area
+  codes vary in length but always begin with 0.
+- Mobile numbers have no parentheses, but have a space in the middle
+  of the number to help readability. The prefix of a mobile number
+  is its first four digits, and they always start with 7, 8 or 9.
+- Telemarketers' numbers have no parentheses or space, but they start
+  with the area code 140.
 
 Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
- <list of codes>
+  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
 
 Part B: What percentage of calls from fixed lines in Bangalore are made
@@ -44,3 +44,31 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+codes = []
+bangalore_count = 0
+total_calls = 0
+
+for _, call in enumerate(calls):
+  caller = call[0]
+  receiver = call[1]
+  code = ''
+  if caller[:5] == '(080)':
+    if receiver[:1] == '(':
+      code = receiver[1:receiver.index(')')]
+    elif receiver[:1] in ['7', '8', '9']:
+      code = receiver[:4]
+    else:
+      code = '140'
+  if code and code not in codes:
+    codes.append(code)
+  if code == '080':
+    bangalore_count += 1
+  total_calls += 1
+
+print("The numbers called by people in Bangalore have codes:")
+for code in sorted(codes):
+  print(code)
+
+
+print(f"\n{bangalore_count/total_calls*100:.2f} percent of calls from fixed lines in\
+ Bangalore are calls to other fixed lines in Bangalore.")
